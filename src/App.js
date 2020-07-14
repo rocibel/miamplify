b975c5ac2574d3f2195599b7135c0651bd4244d0
rocibel <61777795+rocibel@users.noreply.github.com>
2020-07-14 13:58:22 +0200

    14
    14

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MiComponenete from './componentes/MiComponente';
+
+
+
+function HolaMundo(nombre,edad){
+  var presentacion=<div>
+    <h2>Hola, soy {nombre}</h2>
+    <h2>Tengo {edad} años</h2>
+  </div>
+  return presentacion;
+ 
+}
+
 
 function App() {
+  var nombre ="Rocibel Alvarado";
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hola Rocibel</h1>
+        <h1 className="App-title">Hola Rocibel hola</h1>
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {
+        HolaMundo(nombre,26)
+        }
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,7 +39,12 @@ function App() {
         >
           Learn React
         </a>
+        <section className="Componentes">
+
+        <MiComponenete/>
+      </section>
       </header>
+      
     </div>
   );
 }
---
+++ src/componentes/MiComponente.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class MiComponente extends Component{
+
+    render(){
+        return(
+            <h1> Hola LLamo al MiComponente</h1>
+            <h2></h2>
+        );
+    }
+}
+
+export default MiComponente;
